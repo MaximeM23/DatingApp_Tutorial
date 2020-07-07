1fd70df73361b8dbd6a910e7db1273333f60282a
MaximeM23 <Maxime.Michel.23@hotmail.com>
2020-07-07 22:55:23 +0200

    End of section 12

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule } from 'ngx-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
@@ -11,7 +11,7 @@ import { FileUploadModule } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
@@ -59,9 +59,12 @@ export class CustomHammerConfig extends HammerGestureConfig {
    ],
    imports: [
       BrowserModule,
+      BrowserAnimationsModule,
       HttpClientModule,
       FormsModule,
+      ReactiveFormsModule,
       BsDropdownModule.forRoot(),
+      BsDatepickerModule.forRoot(),
       BrowserAnimationsModule,
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,15 +1,42 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
-        [Required]        
-        [StringLength(20,MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
-        public string Username { get; set; }
-
+        public UserForRegisterDto(string username, string password, string gender,string knownAs, string city, string country, DateTime dateOfBirth)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Gender = gender;
+            this.DateOfBirth = dateOfBirth;
+            this.KnownAs = knownAs;
+            this.City = city;
+            this.Country = country;
+        }
+        
         [Required]
-        [StringLength(20,MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string KnownAs { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }   
+        [Required]     
+        public string City { get; set; }
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+
+        public UserForRegisterDto() {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
